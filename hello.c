65d00d2fe5627d5e54f149f84c2e3a49eb9b1866
mfilter3 <111907334+mfilter3@users.noreply.github.com>
2022-08-25 20:08:06 +0000

    initial commit

---
+++ hello.c
@@ -0,0 +1,18 @@
+/**
+ * Author: Mason Filter, Tyler Rayl
+ *         mfilter@huskers.unl.edu
+ * Date: 2022/08/23
+ *
+ * A simple hello world program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Hello World!\n");
+  printf("Mason Filter\n");
+
+  return 0;
+}
